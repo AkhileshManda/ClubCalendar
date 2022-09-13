ebae8b03ac5000a204a1b760a09208ac8cd57a72
AkhileshManda <manda.akhilesh4@gmail.com>
2022-09-13 13:47:01 +0530

    Added user club_calendar app

---
+++ club_calendar/lib/features/app_review/view/widgets/rating_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import '../../../../styles.dart';
+import '../../controller/app_feedback_provider.dart';
+import 'package:provider/provider.dart';
+
+Widget RatingCard(String title,context, review) {
+  var styles = new Styles();
+  return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 0,
+        color: Styles.backgroundColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: styles.cardHeadingStyle(),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: RatingBar.builder(
+                initialRating: title =="Overall Experience"? (review.overallRating == null? 3: review.overallRating): (review.experienceRating == null? 3: review.experienceRating),
+                minRating: 0,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                itemBuilder: (context, _) => const Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                unratedColor: Colors.white,
+                onRatingUpdate: (rating) {
+                   if(title == "Overall Experience"){
+                    review.setOverallRating(rating);
+                   }else{
+                     review.setExperienceRating(rating);
+                   }
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  
+}
